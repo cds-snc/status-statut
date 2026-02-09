a5c7bd48b5146479b05e3a5c2b4b8562ac6f04ee
Pat Heard <patrick.heard@cds-snc.ca>
2026-02-09 13:47:47 -0500

    feat: switch runner to CodeConnection
    Remove the GitHub PAT and switch to using a CodeConnection to authenticate
    CodeBuild with GitHub. The CodeConnection uses the AWS managed GitHub app
    that is installed in our org.

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,9 +1,3 @@
-variable "github_personal_access_token" {
-  description = "The GitHub personal access token to use for the CodeBuild project"
-  type        = string
-  sensitive   = true
-}
-
 variable "region" {
   description = "The AWS region to deploy to"
   type        = string
