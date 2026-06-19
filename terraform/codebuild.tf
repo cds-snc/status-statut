7acaf7f73142cfc4f8c124c4f5c9b2b81f8eb69e
Sylvia McLaughlin <sylvia.mclaughlin@cds-snc.ca>
2026-06-19 15:26:41 -0700

    Upgrading provider to get all the latest tags

--- terraform/codebuild.tf
+++ terraform/codebuild.tf
@@ -1,5 +1,5 @@
 module "github_runner" {
-  source = "github.com/cds-snc/terraform-modules//codebuild_github_runner?ref=v10.11.4"
+  source = "github.com/cds-snc/terraform-modules//codebuild_github_runner?ref=v11.3.5"
 
   project_name               = "cds-snc-status-statut"
   github_repository_url      = "https://github.com/cds-snc/status-statut.git"
