8f0d82e0b21230f9274da06efec25793be199988
Sylvia McLaughlin <85905333+sylviamclaughlin@users.noreply.github.com>
2026-06-22 10:58:17 -0700

    Update ssc_cbrid tags for status-status (#485)
    * Upgrading provider to get all the latest tags
    
    * Upgrading provider and module version to get latest tags

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -9,4 +9,19 @@ terraform {
 
 provider "aws" {
   region = var.region
+  default_tags {
+    tags = {
+      ssc_cbrid = "22DI"
+    }
+  }
+}
+
+provider "aws" {
+  alias  = "core_services"
+  region = var.region
+  default_tags {
+    tags = {
+      ssc_cbrid = "22DH"
+    }
+  }
 }
